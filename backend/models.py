194930e3d40b53340ced22459f3d9486898daee2
cpusillo <cpusillo@gmail.com>
2024-04-18 17:18:37 -0400

    Update models

--- backend/models.py
+++ backend/models.py
@@ -1,6 +1,11 @@
 from config import db
 
 class Contact(db.Model):
+    """
+    Class Contact forms the database model which will
+    store all of our "contacts"
+    @params = first_name, last_name, email
+    """
     # Database model represented as a class
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(80), unique=False, nullable=False)
@@ -12,4 +17,10 @@ class Contact(db.Model):
         Takes our DB fields and converts them into dict objects
         which can be fed as JSON into our API.
         """
+        return {
+            "id": self.id,
+            "firstName": self.first_name,
+            "lastName": self.last_name,
+            "email": self.email
+        }
 
